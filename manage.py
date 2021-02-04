bef8bbc0887f3e1ea0987656ebfd212ccd9fc487
KidsXH <wlzeign@hotmail.com>
2021-02-04 13:49:12 +0800

    CUDA

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FLHeteroBackend.settings')
+    os.environ['CUDA_VISIBLE_DEVICES'] = "0"
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
